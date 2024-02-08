51e432b978517357188eb011d57761b3c668443a
Raymond Warnod <raymond.warnod@eleve.ensai.fr>
2024-02-08 16:57:05 +0100

    debutFonction

--- script2.R
+++ script2.R
@@ -7,18 +7,55 @@ rm(list=ls())
 
 
 #setwd("~/Documents/projet_stat/data") Camille
-#setwd("~/Cours2A/projetStat2/projetStat2A/data") Raymond
+setwd("~/Cours2A/projetStat2/projetStat2A/data") #Raymond
 library(dplyr)
 library(ggplot2)
 library(lmtest)
 
+fModele <- function(cheminCsv, nomX = names(etalonnage)[1], nomY= names(etalonnage)[2]){
+  etalonnage <- read.csv2(paste0(cheminCsv, ".csv"), sep = ",")
+  print("Summary :")
+  print( summary(etalonnage) )
+  modele <- lm(etalonnage[,1]~etalonnage[,2])
+  print(paste0("Regression linéaire de la ",nomY,"en fonction de ",nomY))
+  print(summary(modele))
+  
+  par(mfrow=c(2,2))
+  plot(modele)
+  par(mfrow=c(1,1))
+  print("Test de Breusch-Pagan")
+  bp_test <- bptest(modele)
+  print(bp_test)
+  
+  alpha = 0.95
+  df = as.numeric(nobs(modele))
+  sigma_y <- summary(modele)$sigma
+  b0 <- coef(modele)[1]
+  b1 <- coef(modele)[2]
+  LOB <-  (qt(1 - alpha/2, df = df)*sigma_y-b0)/b1
+  print(paste0("Limite de blanc: ", LOB))
+  
+  
+  ggplot(etalonnage) +
+    aes(x = etalonnage[,1], y = etalonnage[,2]) +
+    #aes(x = Concentration, y = DO) +
+    geom_point(colour = "red", alpha = 1) +
+    labs(x = nomX, y = nomY) +
+    geom_smooth(method = "lm") +
+    #  geom_smooth(method = "lm", formula = y ~ poly(x, 2), se = T, color = "green") +
+    theme_light()
+}
 
-etalonnage = read.csv2("etalonnage_9_02_23.csv", sep = ",")
+fModele("etalonnage_color")
 
+
+
+#etalonnage = read.csv2("etalonnage_9_02_23.csv", sep = ",")
+etalonnage = read.csv2("etalonnage_color.csv", sep = ",")
 summary(etalonnage)
 
 ggplot(etalonnage) +
-  aes(x = Concentration, y = Temps) +
+  aes(x = Concentration, y = DO) +
   geom_point(colour = "red", alpha = 1) +
   labs(x = "Concentration", y = "Temps") +
   geom_smooth(method = "lm") +
@@ -27,6 +64,8 @@ ggplot(etalonnage) +
 
 modele <- lm(Concentration~., data = etalonnage)
 summary(modele)
+
+
 par(mfrow=c(2,2))
 plot(modele)
 par(mfrow=c(1,1))
@@ -60,7 +99,10 @@ summary(modele2)
 
 #III
 
-etalonnage3 = read.csv2("etalonnage_9_02_23.csv", sep = ",")
+#etalonnage3 = read.csv2("etalonnage_9_02_23.csv", sep = ",")
+etalonnage3 = read.csv2("etalonnage_color.csv", sep = ",")
+
+
 etalonnage3$logDO <- log(1+etalonnage3$DO)
 summary(etalonnage3)
 
