ba18b510c2428050dc779eb2ab0ee2c18922f983
Raymond Warnod <raymond.warnod@eleve.ensai.fr>
2024-02-23 15:06:40 +0100

    cas Hétérogène

--- script2.R
+++ script2.R
@@ -65,7 +65,8 @@ fModele <- function(
 
   # calcul du LOB à partir de l'étalon
   n <- nrow(etalonnage)
-  sigma_y <- sd(etalonnage[,2])
+  #sigma_y <- sd(etalonnage[,2])
+  sigma_y <- sigma(modele)
   dll <- n - 2
   quantileStudent <- qt(1 - alpha, df = dll)
   b1ab <- abs(b1)
@@ -162,13 +163,125 @@ fichiersArgatroban <- c("argatrobanEtalonAvril.csv",
 Argatroban <-do.call("rbind", Map(fModele, fichiersArgatroban))
 
 
-
-
-
 Argatroban <-do.call("rbind", 
                      Map(function (x) fModele(x, precisionRelative = 0.5), 
-                         fichiersArgatroban)
-                     )
+                         fichiersArgatroban) )
+
+# Modele hétérogene
+
+fModeleHetero <- function(
+  # chemin des données d'étalonnage x = col1 et y = col2
+  cheminCsv,
+  # C'est de le nom des axes pour la représentation graphique
+  nomX = names(etalonnage)[1],
+  nomY= names(etalonnage)[2],
+  # c'est le alpha pour les quantiles  de la loi de Student
+  alpha = 0.05,
+  # Precision absolue pour le calcul de la limite de quantification
+  precision = 10,
+  # Precision relative pour le calcul de la limite de quantification
+  precisionRelative = sd(etalonnage[,1])/ mean(etalonnage[,1])
+){
+  etalonnage <- read.csv2(cheminCsv)
+  
+  #Modele generalisé
+  model<-glm(etalonnage[,2]~etalonnage[,1], family=gaussian(link="identity"))
+  print(paste0("Regression linéaire de ", nomY," en fonction de ",nomX))
+  print(summary(model))
+  b0 <- coef(model)[1]
+  b1 <- coef(model)[2]
+  
+  # calcul du LOB à partir de l'étalon
+  n <- nrow(etalonnage)
+  sigma_y <- sigma(model)
+  dll <- n - 2
+  quantileStudent <- qt(1 - alpha, df = dll)
+  b1ab <- abs(b1)
+  S1surX <- sum(1/(etalonnage[,1] + 0.01)) # comment on fait pour les zeros
+  SX <- sum(etalonnage[,1])
+  D <- n*sum(etalonnage[,1]^2) - SX^2 # a modifier
+  A <- quantileStudent * (sigma_y/b1ab)
+  LOB <- A * ( 1/S1surX + (n*n)/(S1surX*D) )^0.5
+  
+  # Calcul des limites de détection
+  
+  quantileStudent <- qt(1 - alpha, df = dll)
+  K <- (quantileStudent*sigma_y/b1)^2
+  S1surX <- sum(1/(etalonnage[,1] + 0.01)) # a modifier
+  
+  a <- (1 - S1surX*K/D)
+  b <- 2*(n*K/D - LOB) - K
+  c <- LOB^2 - (K/S1surX)*(1 + n*n/D)
+  
+  delta <- b^2 - 4*a*c
+  print(paste0("Delta LID: ", delta))
+  if (delta<0){
+    LID1 <- 0
+    LID2 <- 0
+  }else{
+    LID1 <- (-b - sqrt(delta)) / (2*a)
+    LID2 <- (-b + sqrt(delta)) / (2*a)
+  }
+  # On calcul la limite de quantification absolue
+  quantileStudent <- qt(1 - alpha/2, df = dll)
+  
+  t <- (quantileStudent*sigma_y/b1)^2
+  a <- t*S1surX/D
+  b <- -2*t*n/D
+  c <- (t/S1surX)*(1 + (n^2)/D) - precision^2
+  delta <- b^2 - 4*a*c
+  print(paste0("Delta LIQ: ", delta))
+  if (delta<0){
+    LIQ1 <- 0
+    LIQ2 <- 0
+  }else{
+    LIQ1 <- (-b - sqrt(delta)) / (2*a)
+    LIQ2 <- (-b + sqrt(delta)) / (2*a)
+  }
+  # On calcul la limite de quantification relative
+  print(paste0("Presicion relative : ", precisionRelative))
+  
+  a <- t*S1surX/D - precisionRelative^2
+  b <- t*(1 - 2*n/D)
+  c <- (t/S1surX)*(1 + (n^2)/D)
+  delta <- b^2 - 4*a*c
+  print(paste0("Delta LIQR: ", delta))
+  if (delta < 0 ){
+    LIQR1 <- 0
+    LIQR2 <- 0
+  }else{
+    LIQR1 <- (-b - sqrt(delta)) / (2*a)
+    LIQR2 <- (-b + sqrt(delta)) / (2*a)
+  }
+  data.frame( b0 = b0, b1 = b1, sigma_y = sigma_y,D=D,
+              LOB= LOB, LID1 = LID1, LID2 = LID2, LIQ1 = LIQ1,
+              LIQ2 = LIQ2, LIQR1 = LIQR1,
+              LIQR2 = LIQR2  )
+  
+}
+
+
+ArgatrobanHetero <-do.call("rbind", Map(fModeleHetero, fichiersArgatroban))
+
+
+ArgatrobanHetero <-do.call("rbind", 
+                     Map(function (x) fModeleHetero(x, precisionRelative = 0.5), 
+                         fichiersArgatroban) )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # les LOB sont trop haut
 
 # Droites de régression:
